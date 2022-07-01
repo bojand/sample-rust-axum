66b8790400f563f1cfff0642f25fc5f98c24578d
Bojan <dbojan@gmail.com>
2022-06-30 22:10:42 -0300

    fix port

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use std::env;
 use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    let port = match env::var("PORT") {
+        Ok(val) => val.parse::<u16>().unwrap(),
+        Err(_e) => 3000,
+    };
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
     // run it
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
